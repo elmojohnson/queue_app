9774c910c93ecf325ce93a149818bed24bceee90
John Elmo Johnson <elmoj6@gmail.com>
2022-09-22 19:10:59 +1000

    View members real-time

--- components/drawers/MembersDrawer.js
+++ components/drawers/MembersDrawer.js
@@ -1,3 +1,9 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+import { db } from "../../utils/firebase";
+import { collection, onSnapshot, query } from "firebase/firestore";
+
 import {
   Drawer,
   DrawerBody,
@@ -8,15 +14,49 @@ import {
   DrawerCloseButton,
   Button,
   useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
+
 import AddMemberModal from "../modals/AddMemberModal";
+import LoadingScreen from "../utils/LoadingScreen";
+import MemberItem from "../items/MemberItem";
 
 const MembersDrawer = ({ isOpen, onClose, btnRef }) => {
+  const [members, setMembers] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  const router = useRouter();
   const {
     isOpen: modalIsOpen,
     onOpen: modalOnOpen,
     onClose: modalOnClose,
   } = useDisclosure();
+
+  const getMembers = async () => {
+    const memQuery = query(
+      collection(db, "rooms", router?.query.id, "members")
+    );
+
+    onSnapshot(memQuery, (qs) => {
+      let arr = [];
+
+      qs.forEach((doc) => {
+        arr.push({
+          member_id: doc.id,
+          spotify_id: doc.data().id,
+          ...doc.data(),
+        });
+      });
+
+      console.log(arr);
+      setMembers(arr);
+    });
+  };
+
+  useEffect(() => {
+    router.isReady && getMembers();
+  }, []);
+
   return (
     <>
       <Drawer
@@ -24,16 +64,29 @@ const MembersDrawer = ({ isOpen, onClose, btnRef }) => {
         placement="right"
         onClose={onClose}
         finalFocusRef={btnRef}
+        size="lg"
       >
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerHeader>Members</DrawerHeader>
 
-          <DrawerBody>asd</DrawerBody>
+          <DrawerBody>
+            {isLoading ? (
+              <LoadingScreen />
+            ) : (
+              <VStack spacing={4}>
+                {members.map((member) => {
+                  return <MemberItem user={member} />;
+                })}
+              </VStack>
+            )}
+          </DrawerBody>
 
           <DrawerFooter>
-            <Button colorScheme="green" onClick={modalOnOpen}>Add</Button>
+            <Button colorScheme="green" onClick={modalOnOpen}>
+              Add
+            </Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
---
+++ components/items/MemberItem.js
@@ -0,0 +1,37 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  HStack,
+  Icon,
+  Spacer,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { MdPersonRemove } from "react-icons/md";
+
+const MemberItem = ({ user }) => {
+  return (
+    <Box w="full">
+      <HStack alignItems="start">
+        <Avatar src={user.image} name={user.name} />
+        <VStack alignItems="start" spacing={0}>
+          <Text fontSize={14} fontWeight="semibold">
+            {user.name}
+          </Text>
+          <Text fontSize={10}>{user.email}</Text>
+        </VStack>
+        <Spacer />
+        <Button
+          size="xs"
+          leftIcon={<Icon as={MdPersonRemove} />}
+          variant="outline"
+        >
+          Remove
+        </Button>
+      </HStack>
+    </Box>
+  );
+};
+
+export default MemberItem;
